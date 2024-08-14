34ec6f602dcd63fb6299f6b30100621c19c02cba
Angelos Ioannou <angelos.ioannou@gmx.de>
2024-08-15 00:25:10 +0200

    added verse command functionality

--- utils.js
+++ utils.js
@@ -1,18 +1,19 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export async function DiscordRequest(endpoint, options) {
   // append endpoint to root API URL
-  const url = 'https://discord.com/api/v10/' + endpoint;
+  const url = "https://discord.com/api/v10/" + endpoint;
   // Stringify payloads
   if (options.body) options.body = JSON.stringify(options.body);
   // Use fetch to make requests
   const res = await fetch(url, {
     headers: {
       Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
-      'Content-Type': 'application/json; charset=UTF-8',
-      'User-Agent': 'DiscordBot (https://github.com/discord/discord-example-app, 1.0.0)',
+      "Content-Type": "application/json; charset=UTF-8",
+      "User-Agent":
+        "DiscordBot (https://github.com/discord/discord-example-app, 1.0.0)",
     },
-    ...options
+    ...options,
   });
   // throw API errors
   if (!res.ok) {
@@ -30,7 +31,7 @@ export async function InstallGlobalCommands(appId, commands) {
 
   try {
     // This is calling the bulk overwrite endpoint: https://discord.com/developers/docs/interactions/application-commands#bulk-overwrite-global-application-commands
-    await DiscordRequest(endpoint, { method: 'PUT', body: commands });
+    await DiscordRequest(endpoint, { method: "PUT", body: commands });
   } catch (err) {
     console.error(err);
   }
@@ -38,10 +39,58 @@ export async function InstallGlobalCommands(appId, commands) {
 
 // Simple method that returns a random emoji from list
 export function getRandomEmoji() {
-  const emojiList = ['😭','😄','😌','🤓','😎','😤','🤖','😶‍🌫️','🌏','📸','💿','👋','🌊','✨'];
+  const emojiList = [
+    "😭",
+    "😄",
+    "😌",
+    "🤓",
+    "😎",
+    "😤",
+    "🤖",
+    "😶‍🌫️",
+    "🌏",
+    "📸",
+    "💿",
+    "👋",
+    "🌊",
+    "✨",
+  ];
   return emojiList[Math.floor(Math.random() * emojiList.length)];
 }
 
 export function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+// Verse command functionality
+const randomNumber = (max, offset = 1) => {
+  return Math.floor(Math.random() * max) + offset;
+};
+
+export const getVerse = async (translation = "deu_sch") => {
+  let displayMessage = "";
+
+  const bookArray = await fetch(
+    `https://bible.helloao.org/api/${translation}/books.json`
+  ).then((res) => res.json());
+
+  // const bookArray = schlachterBibelBooks;
+
+  const randomBook = bookArray.books[randomNumber(66)];
+
+  const randomChapter = randomNumber(randomBook.numberOfChapters);
+
+  const getRandomChapter = await fetch(
+    `https://bible.helloao.org/api/${translation}/${randomBook.id}/${randomChapter}.json`
+  ).then((res) => res.json());
+
+  const verseArray = getRandomChapter.chapter.content;
+  const amountVerses = getRandomChapter.numberOfVerses;
+
+  const randomVerse = verseArray[randomNumber(amountVerses, 0)];
+
+  displayMessage = `${getRandomChapter.book.name} ${randomChapter}:${randomVerse.number} - ${randomVerse.content[0]}`;
+  return displayMessage;
+};
+
+getVerse();
